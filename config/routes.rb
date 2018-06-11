b77b31d0dbbb7dd6b155b8f8f6557cde2d562b94
Alexander Rowland <arowland900@gmail.com>
2018-06-11 11:47:10 -0700

    finished log in / log out with notifications

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  root "pages#home"
+  root 'users#index'
+  resources :users
+
+  resources :sessions, only: [:new, :create]
+  delete '/logout' => 'sessions#destroy', as: :logout
 end
